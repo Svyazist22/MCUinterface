bbbe6362381e2ccc61d4853f9a46bb47bc933fe8
Svyazist22 <bakanov.vp.22@gmail.com>
2021-09-10 00:27:55 +0300

    add methods

--- interface.cpp
+++ interface.cpp
@@ -1,1 +1,90 @@
 #include "interface.h"
+
+void Interface::writeByte(uint8_t byte)
+{
+    start();
+    for(int i = 0; i < 8; i++)
+    {
+        if (byte & 0x80)
+        _data = 1;
+        else
+        _data = 0;
+        byte << 1;
+
+        _sck = 1; 
+        /* convert byte */
+        //if (_data)
+        //byte |= 0x01;
+        _sck = 0; 
+    }
+    stop();
+}
+
+
+void Interface::writeMsg(char *data, uint8_t len)
+{
+    Interface::mode(spi_mode_t::MODE_WRITE);
+    for (int8_t i = 0; i < len; i++)
+    {
+        writeByte((uint8_t)data[i]);
+    }
+}
+
+
+void Interface::read(char *data)
+{
+    int size = 0;
+    uint8_t byte;
+    Interface::mode(spi_mode_t::MODE_READ);
+    start();
+    while (!_ce)
+    {
+        byte = 0;
+        for (int i = 0; i < 8; i++)
+        {
+            if (_data)
+            {
+                byte |= 0x01;
+                byte << 1;
+            }
+            else
+            {
+                byte << 1;
+            }
+        }
+        data[size] = (char)byte;
+    }
+    stop();
+}
+
+
+void Interface::mode(int mode)
+{
+    if (mode)
+    {
+        Interface::_wr = 1;
+    }
+    else
+    {
+        Interface::_wr = 0;
+    }
+}
+
+
+void Interface::start()
+{
+    Interface::_sck = 0;
+    Interface::_ce = 0;
+}
+
+
+void Interface::stop()
+{
+    Interface::_ce = 1;
+}
+
+
+void Interface::reset()
+{
+    Interface::_res = 1;
+}
--- interface.h
+++ interface.h
@@ -1,11 +1,30 @@
-
+#include <cstdint>
 
 class Interface
 {
+    enum spi_mode_t
+    {
+        MODE_WRITE = 0,
+        MODE_READ = 1
+    };
+
 private:
-  
+
+    uint8_t _sck;   // serial clock
+    uint8_t _data;  // data line
+    uint8_t _res;   // reset
+    uint8_t _wr;    // select mode (write/read)
+    uint8_t _ce;    // chip enable
+
 public:
-  
+
+    void read(char *data);
+    void writeByte(uint8_t byte);
+    void writeMsg(char *data, uint8_t len);
+    void start();           // sck 0, ce 0
+    void stop();            // ce 1
+    void mode(int mode);    // 0 - write, 1 - read
+    void reset();           // reset = 1
 };
 
 
